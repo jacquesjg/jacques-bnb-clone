e3d5423604b13f7e5e34fd837cd1ebcee517764d
jacquesjg <jjeangi@gmail.com>
2021-12-08 16:36:43 +0100

    fixed home searchbox css

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './Searchbox.css'
 function Searchbox() {
   return (
     <div className="Searchbox__Container">
-
+      <div className="Searchbox__Contents">
+        <div classname="Searchbox__Title">
+          Book Unique Experiences
+        </div>
+        testtesttesttesttesttesttesttest
+        testtesttesttesttesttesttesttesttest
+        testtesttest
+      </div>
     </div>
   )
 };
