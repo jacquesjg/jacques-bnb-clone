48e28eee9d6b6ba34af491af5b7d97a1571fc8c0
jacquesjg <jjeangi@gmail.com>
2021-12-12 17:49:15 +0100

    added react-dates

--- src/components/Searchbox/Searchbox.js
+++ src/components/Searchbox/Searchbox.js
@@ -4,12 +4,29 @@ function Searchbox() {
   return (
     <div className="Searchbox__Container">
       <div className="Searchbox__Contents">
-        <div classname="Searchbox__Title">
-          Book Unique Experiences
-        </div>
-        testtesttesttesttesttesttesttest
-        testtesttesttesttesttesttesttesttest
-        testtesttest
+        <span id="Searchbox__Title">Book unique homes</span>
+        <form>
+          <span id="Searcbox__Input__Label">WHERE</span>
+          <input name="destination" className="Home__Search__Inputs" type="text" placeholder="Try New York" autoComplete="new-password" />
+          <span id="Searcbox__Input__Label">DATES</span>
+          <input className="Home__Search__Inputs" name="destination" type="text" autoComplete="new-password" />
+          <span id="Searcbox__Input__Label">GUESTS</span>
+          <select id="guests" className="Home__Search__Inputs" >
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+          </select>
+
+
+          <button id="sign__button">Search</button>
+        </form>
       </div>
     </div>
   )
