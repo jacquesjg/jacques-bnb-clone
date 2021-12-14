7381447c4613d0301d5a080a4e0d12b3c1607c21
jacquesjg <jjeangi@gmail.com>
2021-12-14 18:33:47 +0100

    gogle maps api and map working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Home from './components/Home/Home';
+import Map from './components/Map/Map';
 import Header from './components/Header/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
         <Header />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/search-result" element={<Map />} />
         </Routes>
       </Router >
 
---
+++ src/components/Map/Map.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
+import axios from 'axios';
+require("dotenv").config();
+
+
+function setMapCenter() {
+  return (
+    <GoogleMap
+      defaultZoom={12}
+      defaultCenter={{ lat: 40.730610, lng: -73.935242 }} />
+  );
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(setMapCenter));
+
+function Map() {
+
+  const [combineListingData, setCombineListingData] = useState([]);
+
+  return (
+    <div>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_API_KEY}`}
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100vh` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+    </div>
+  )
+}
+
+export default Map
