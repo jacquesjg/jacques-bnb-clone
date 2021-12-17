6cd6b4f1d18bf1dcf0c1f34d882ad9e456b675b5
jacquesjg <jjeangi@gmail.com>
2021-12-17 08:15:15 +0100

    listing page basic layout complete

--- src/components/Listing/Listing.js
+++ src/components/Listing/Listing.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { SearchContext } from '../../context/searchContext';
 import axios from 'axios';
-
-// import { Wifi, LocalParking, Kitchen, Tv, Microwave, OutdoorGrill, HotTub, DateRange, LocalLaundryService, AcUnit, Hvac } from '@mui/icons-material';
+import { StarOutline, Wifi, LocalParking, Kitchen, Tv, Microwave, OutdoorGrill, HotTub, DateRange, LocalLaundryService, AcUnit, Hvac } from '@mui/icons-material';
+import { Container, Box } from '@mui/material';
 import { useParams } from 'react-router-dom';
 import { DataContext } from '../../App';
+import './Listing.css';
 var moment = require('moment');
 
 
@@ -25,6 +26,7 @@ function Listing() {
   const fetchListingData = async () => {
     try {
       const result = await axios.get('https://jacquesjg.github.io/jacques-bnb-clone-api2/listingAll.json');
+
       setListings(result.data)
     } catch (error) {
       console.log("error:", error.message);
@@ -37,19 +39,70 @@ function Listing() {
   // console.log(b.diff(a, 'days'));
 
   return (
-    <div>
+    <div className='listings__page__container'>
 
-      {console.log(guests)}
-      {listings ? listings.map(listing =>
-        <div key={listing.id}>{listingID}</div>
+
+      {listings ? listings.map((listing, index) =>
+        listing.id == listingID ?
+          <Container
+            key={index}
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+
+
+            <div className="my_info">
+              {listing.name}
+              <div className="city__rating">
+                <StarOutline style={{ color: "#FF5A5F", fontSize: "16px", lineHeight: "20px !important" }} />
+                {listing.review_scores_rating ? `Rating: ${listing.review_scores_rating}` : null} {listing.neighbourhood ? `• ${listing.neighbourhood}` : null}
+              </div>
+            </div>
+
+
+            <Box
+              sx={{
+                height: 570,
+                width: 975,
+                borderRadius: 8,
+                backgroundImage: `url(${listing.picture_url})`,
+                backgroundPosition: "center",
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "cover"
+              }}
+            />
+
+            <div className="left__right__container">
+
+              <div className="left__container">
+                <div className="type__and__hood">
+                  {listing.property_type} in {listing.neighbourhood_cleansed}
+                </div>
+              </div>
+
+              <div className="right__container">
+                test
+              </div>
+
+
+
+            </div>
+
+
+
+          </Container>
+          : null
       ) : null}
 
+
       {/* 
-      {console.log(currentListing.picture_url)}
+
       {console.log(currentListing.property_type)}
       {console.log(currentListing.neighbourhood_cleansed)}
-      {console.log(currentListing.name)}
-      {console.log(currentListing.neighbourhood)}
+
       {console.log(currentListing.description)}
       {console.log(currentListing.bedrooms)}
       {console.log(currentListing.beds)}
@@ -57,8 +110,8 @@ function Listing() {
       {console.log(currentListing.amenities)}
       {console.log(currentListing.price)}
       {console.log(currentListing.review_scores_rating)} */}
-      {/* 
-      <Wifi />
+
+      {/* <Wifi />
       <LocalParking />
       <Kitchen />
       <Tv />
